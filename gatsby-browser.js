0dadf485fca95b9aaeb3884c28c39aa0590d3aef
Marta Banaś <65865826+martam90@users.noreply.github.com>
2021-06-05 13:35:45 +0200

    Scss styles (#1)
    * add scss styles
    
    * add scss header styles
    
    * add scss footer styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,2 @@
 import './src/styles/index.scss';
-import './src/components/Header/index.scss';
-import './src/components/Footer/index.scss';
-import './src/components/Main/index.scss';
-import './src/components/AboutCastle/index.scss';
+
