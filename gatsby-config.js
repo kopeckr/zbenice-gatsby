91a7adfe07d392dbbc91ac0b09ad118e2a7fae4f
Marta Banas <banas.m.marta@gmail.com>
2021-06-05 11:32:08 +0200

    add scss styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,10 @@ module.exports = {
     title: 'Zbenice',
   },
   plugins: [
-    'gatsby-plugin-sass',
+    { resolve: 'gatsby-plugin-sass', options: { 
+      sassOptions:
+      {data: `@import "${__dirname}/src/styles/abstracts/_variables.scss";`, }
+    } },
     'gatsby-plugin-gatsby-cloud',
     {
       resolve: 'gatsby-plugin-react-svg',
