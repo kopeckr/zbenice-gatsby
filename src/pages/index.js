20b627e23639aa3f402a19f03c03a8d7b15093e5
Marta Banas <banas.m.marta@gmail.com>
2021-06-05 19:46:10 +0200

    add styles adj

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import Header from '../components/Header';
-// import MainNav from '../components/MainNav';
 import Footer from '../components/Footer';
 import Main from '../components/Main';
 
 const IndexPage = () => {
   return (
     <>
-      <a href="#main" class="skip-links">
+      <a href="#main" className="skip-links">
         Přeskočit na obsah
       </a>
       <Header />
-      {/* <MainNav /> */}
       <Main />
       <Footer />
     </>
