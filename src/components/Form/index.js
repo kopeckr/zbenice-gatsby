36d1e2e77fee528b50878d987d77b12dac6bd0b8
Marta Banas <banas.m.marta@gmail.com>
2021-06-06 11:57:53 +0200

    add emailjs

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-// import emailjs from 'emailjs-com';
+import emailjs from 'emailjs-com';
 
 const Form = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_test', 'template_up8mf4v', e.target, 'user_FOX4Z3T8BvsoSsNBwxtcA')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
 
   return (
     <>
-      {/* <form className="contact-form" onSubmit={sendEmail}>
+      <form className="contact-form" onSubmit={sendEmail}>
         <input type="hidden" name="contact_number" />
         <label>Name</label>
         <input type="text" name="user_name" />
@@ -14,7 +23,7 @@ const Form = () => {
         <label>Message</label>
         <textarea name="message" />
         <input type="submit" value="Send" />
-    </form> */}
+    </form>
     </>
   );
 }
