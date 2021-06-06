a1da8693420739ede4a6e7b16f83eff302e4e143
Marta Banas <banas.m.marta@gmail.com>
2021-06-06 14:09:23 +0200

    add htmlFor

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -18,15 +18,15 @@ const Form = () => {
       <h2>Kontaktní formulář</h2>
       <form className="contact__form" onSubmit={sendEmail}>
         <input type="hidden" name="contact__number" />
-        <label for="name" className="contact__form--label">Jméno a přijmení
+        <label htmlFor="name" className="contact__form--label">Jméno a přijmení
         <input type="text" name="user__name" id="name" className="contact__form--input"/>
         </label>
         
-        <label for="email" className="contact__form--label">E-mail
+        <label htmlFor="email" className="contact__form--label">E-mail
         <input type="email" name="user__email" id="email" className="contact__form--input"/>
         </label>
       
-        <label for="subject" className="contact__form--label">Vaše zpráva
+        <label htmlFor="subject" className="contact__form--label">Vaše zpráva
         <textarea name="message" id="subject" rows="8" cols="50" className="contact__form--input"/>
         </label>
         <input type="submit" aria-label="Odeslat zprávu" className="cta" value="Odeslat zprávu" />
