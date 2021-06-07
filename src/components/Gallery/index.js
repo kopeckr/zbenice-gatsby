6584a520bc160a89d35e1f46e2ede5197d0fdbb4
Marta Banas <banas.m.marta@gmail.com>
2021-06-07 22:36:13 +0200

    fix gatsby browser

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LightGallery from 'lightgallery/react';
+import { isBrowser } from '../../utils/browser';
 
 import 'lightgallery/scss/lightgallery.scss';
 import 'lightgallery/scss/lg-zoom.scss';
@@ -13,19 +14,33 @@ import images from './gallery-one';
 import imagesTwo from './gallery-two';
 
 const Gallery = () => {
+  const gallery1 = isBrowser && (
+    <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
+        {images.map((image) => 
+          <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
+          <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
+        </a>
+        )}
+    </LightGallery>
+  );
+
+  const gallery2 = isBrowser && (
+    <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
+    {imagesTwo.map((image) => 
+      <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
+      <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
+    </a>
+    )}
+    </LightGallery>
+  );
+
   return (
     <main className="container" id="main">
     <section className="gallery__top">
       <h1>Fotogalerie</h1>
       <h2>Jaro na Zámku Zbenice</h2>
       <div>
-        <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
-        {images.map((image) => 
-          <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
-          <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
-        </a>
-        )}
-        </LightGallery>
+        {gallery1}
       </div>
     </section>
 
@@ -33,13 +48,7 @@ const Gallery = () => {
       <h2>Expozice na Zámku Zbenice</h2>
       <p>V naší sbírce najdete přibližně 200 let stáre kousky, mimo jiné první vydání Dějin Českého národa od Františka Palackého nebo kojeneckou lahvičku z 19. století.</p>
       <div>
-      <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
-        {imagesTwo.map((image) => 
-          <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
-          <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
-        </a>
-        )}
-        </LightGallery>
+        {gallery2}
       </div>
     </section>
     
