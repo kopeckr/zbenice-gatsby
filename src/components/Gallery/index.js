f3c93be41b08e909d24a4e8622b168554249e8f5
Marta Banas <banas.m.marta@gmail.com>
2021-06-07 20:59:12 +0200

    Revert "fix gallery server rendering"
    This reverts commit 90bde133c340040db22e2c764024bb560d82689d.

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -11,42 +11,21 @@ import lgZoom from 'lightgallery/plugins/zoom';
 import lgFullscreen from 'lightgallery/plugins/fullscreen';
 import images from '../../gallery-one.js';
 import imagesTwo from '../../gallery-two.js';
-import { isBrowser } from '../../utils/browser';
 
 const Gallery = () => {
-  let gallery1;
-  let gallery2;
-
-  if (isBrowser) {
-    gallery1 = (
-      <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
-        {images.map((image) => 
-          <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
-          <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
-        </a>
-        )}
-        </LightGallery>
-    );
-
-    gallery2 = (
-      <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
-        {imagesTwo.map((image) => 
-          <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
-          <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
-        </a>
-        )}
-        </LightGallery>
-    );
-  }
-
-
   return (
     <main className="container" id="main">
     <section className="gallery__top">
       <h1>Fotogalerie</h1>
       <h2>Jaro na Zámku Zbenice</h2>
       <div>
-        {gallery1}
+        <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
+        {images.map((image) => 
+          <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
+          <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
+        </a>
+        )}
+        </LightGallery>
       </div>
     </section>
 
@@ -54,7 +33,13 @@ const Gallery = () => {
       <h2>Expozice na Zámku Zbenice</h2>
       <p>V naší sbírce najdete přibližně 200 let stáre kousky, mimo jiné první vydání Dějin Českého národa od Františka Palackého nebo kojeneckou lahvičku z 19. století.</p>
       <div>
-      {gallery2}
+      <LightGallery speed={500} plugins={[lgThumbnail, lgZoom, lgFullscreen]} mode="lg-fade">
+        {imagesTwo.map((image) => 
+          <a href={image.source} key={image.id} className="gallery__item" data-sub-html={image.author}>
+          <img alt={image.alt} src={image.source} className="gallery__item-img" loading="lazy"/>
+        </a>
+        )}
+        </LightGallery>
       </div>
     </section>
     
