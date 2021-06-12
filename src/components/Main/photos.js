7693c381309449b8ab7a65753d941f95aa0b97d2
Kristýna Kopečková <kopeckova92@gmail.com>
2021-06-12 19:57:56 +0200

    changed source photo.js

--- src/components/Main/photos.js
+++ src/components/Main/photos.js
@@ -2,23 +2,23 @@
 //   {
 //     id: 1,
 //     source: '/images/zbenice-phone-55.jpg',
-//     sourceSet: `${photoBottomMobile1} 650w, ${photoBottomDesktop1} 1440w`,
+//     // sourceSet: `${photoBottomMobile1} 650w, ${photoBottomDesktop1} 1440w`,
 //     alt: 'Fotka zámku Zbenice',
 //     class: 'main__photos main__photos-first',
 //   },
 //   {
 //     id: 2,
 //     source: '/images/zbenice-phone-42.jpg',
-//     sourceSet: `${photoBottomMobile1} 650w, ${photoBottomDesktop1} 1440w`,
+//     // sourceSet: `${photoBottomMobile1} 650w, ${photoBottomDesktop1} 1440w`,
 //     alt: 'Fotka zámku Zbenice',
-//     class: 'main__photos main__photos-first',
+//     class: 'main__photos',
 //   },
 //   {
 //     id: 3,
 //     source: '/images/zbenice-phone-52.jpg',
-//     sourceSet: `${photoBottomMobile1} 650w, ${photoBottomDesktop1} 1440w`,
+//     // sourceSet: `${photoBottomMobile1} 650w, ${photoBottomDesktop1} 1440w`,
 //     alt: 'Fotka zámku Zbenice',
-//     class: 'main__photos main__photos-first',
+//     class: 'main__photos',
 //   },
 // ];
 
