30d73b5b9001011f3f13ed101153cfef76022b09
martam90 <banas.m.marta@gmail.com>
2021-10-09 17:34:52 +0200

    update img format

--- src/components/AboutCastle/photos.js
+++ src/components/AboutCastle/photos.js
@@ -1,19 +1,19 @@
 const historicPhotos = [
   {
     id: 1,
-    source: '/images/zbenice-old-1.jpg',
+    source: '/images/zbenice-old-1.webp',
     alt: 'Historická fotka zámku Zbenice',
     class: 'section__photo section__photo-first',
   },
   {
     id: 2,
-    source: '/images/zbenice-old-2.jpg',
+    source: '/images/zbenice-old-2.webp',
     alt: 'Historická fotka zámku Zbenice',
     class: 'section__photo',
   },
   {
     id: 3,
-    source: '/images/zbenice-old-3.jpg',
+    source: '/images/zbenice-old-3.webp',
     alt: 'Historická fotka zámku Zbenice',
     class: 'section__photo',
   },
